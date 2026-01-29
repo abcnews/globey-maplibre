25e0b250aa5f73130bb27658552518405495cf71
Ash Kyd <kyd.ashley@abc.net.au>
2026-01-29 14:39:52 +1000

    feat: add prefersReducedMotion store

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -2,3 +2,16 @@ import { writable } from 'svelte/store';
 
 /** Used in the builder to set the location without flyTo, e.g. in things like lon/lat sliders */
 export const disableMapAnimation = writable(false);
+
+/** Does the user prefer reduced motion, or have they toggled it it with interactive-plugins */
+const prefersReducedMotion = writable(document.body.classList.contains('is-reduced-motion'));
+
+const observer = new MutationObserver(function (event) {
+  prefersReducedMotion.set(document.body.classList.contains('is-reduced-motion'));
+});
+observer.observe(document.body, {
+    attributes: true,
+    attributeFilter: ['class'],
+    childList: false,
+    characterData: false
+});
