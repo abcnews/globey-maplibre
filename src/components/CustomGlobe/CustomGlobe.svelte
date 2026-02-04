de8870edecf0b070060ad27eca6ea37e511a52d3
Ash Kyd <kyd.ashley@abc.net.au>
2026-02-04 14:57:59 +1000

    feat: new tiles url

--- src/components/CustomGlobe/CustomGlobe.svelte
+++ src/components/CustomGlobe/CustomGlobe.svelte
@@ -72,7 +72,7 @@
       <HighlightCountriesHandler highlightCountries={options.highlightCountries} />
     {:else if options.base === 'satellite'}
       <MapRasterHandler
-        url={'https://abcnewsdata.sgp1.digitaloceanspaces.com/map-raster-tiles-bluemarble-hd/{z}/{x}/{y}.webp'}
+        url={'https://abcnewsdata.sgp1.digitaloceanspaces.com/map-raster-tiles-bluemarge/{z}/{x}/{y}.webp'}
         maxZoom={7}
         attribution="NASA Blue Marble"
       />
