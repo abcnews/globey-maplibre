a722b36b8832451de56b33e3c6f2125f156bb618
Ash Kyd <kyd.ashley@abc.net.au>
2026-02-04 15:56:27 +1000

    feat: custom globe black background/classes

--- src/components/CustomGlobe/CustomGlobe.svelte
+++ src/components/CustomGlobe/CustomGlobe.svelte
@@ -23,68 +23,87 @@
   let { rootElStyle, interactive, onLoad, options, children }: Props = $props();
 
   let mapInstance = $state<maplibregl.Map | null>(null);
+
+  const isSatellite = $derived(options.base === 'satellite');
+  const isVectorLight = $derived(options.base === 'street' || options.base === 'countries');
 </script>
 
-<MapLibreLoader
-  {rootElStyle}
-  onLoad={async ({ rootNode, maplibregl }) => {
-    rootNode.style.opacity = '0';
-    const map = new maplibregl.Map({
-      zoom: options.z || 3,
-      minZoom: -1,
-      maxZoom: MAX_ZOOM,
-      attributionControl: false,
-      dragRotate: false,
-      doubleClickZoom: false,
-      style: {
-        version: 8,
-        sources: {},
-        layers: [{ id: 'background', type: 'background', paint: { 'background-color': '#000' } }],
-        sprite: 'https://www.abc.net.au/res/sites/news-projects/map-vector-style-bright/sprite',
-        glyphs: 'https://www.abc.net.au/res/sites/news-projects/map-vector-fonts/{fontstack}/{range}.pbf'
-      },
-      container: rootNode,
-      interactive: !!interactive,
-      center: options.coords
-    });
-
-    await Promise.all([new Promise(resolve => map.on('load', resolve))]);
-    map.setProjection({
-      type: 'globe' // Set projection to globe
-    });
-    await new Promise(resolve => map.on('idle', resolve));
-    onLoad?.(map);
-
-    rootNode.style.opacity = '1';
-    mapInstance = map;
-    return map;
-  }}
+<div
+  class="custom-globe"
+  class:custom-globe--satellite={isSatellite}
+  class:custom-globe--vector-light={isVectorLight}
+  style={rootElStyle}
 >
-  {#if mapInstance}
-    <PanZoomHandler coords={options.coords} z={options.z} bounds={options.bounds} />
-    {#if options.base !== 'satellite'}
-      <MapLabelHandler labels={options.mapLabels} isSatellite={options.base === 'satellite'} />
-    {/if}
-    <MapCustomLabelHandler labels={options.labels} />
+  <MapLibreLoader
+    {rootElStyle}
+    onLoad={async ({ rootNode, maplibregl }) => {
+      rootNode.style.opacity = '0';
+      const map = new maplibregl.Map({
+        zoom: options.z || 3,
+        minZoom: -1,
+        maxZoom: MAX_ZOOM,
+        attributionControl: false,
+        dragRotate: false,
+        doubleClickZoom: false,
+        style: {
+          version: 8,
+          sources: {},
+          layers: [{ id: 'background', type: 'background', paint: { 'background-color': '#000' } }],
+          sprite: 'https://www.abc.net.au/res/sites/news-projects/map-vector-style-bright/sprite',
+          glyphs: 'https://www.abc.net.au/res/sites/news-projects/map-vector-fonts/{fontstack}/{range}.pbf'
+        },
+        container: rootNode,
+        interactive: !!interactive,
+        center: options.coords
+      });
 
-    {#if options.base === 'countries'}
-      <MapCountriesBaseHandler />
-      <HighlightCountriesHandler highlightCountries={options.highlightCountries} />
-    {:else if options.base === 'satellite'}
-      <MapRasterHandler
-        url={'https://abcnewsdata.sgp1.digitaloceanspaces.com/map-raster-tiles-bluemarge/{z}/{x}/{y}.webp'}
-        maxZoom={7}
-        attribution="NASA Blue Marble"
-      />
-    {:else}
-      <MapVectorHandler />
-    {/if}
+      await Promise.all([new Promise(resolve => map.on('load', resolve))]);
+      map.setProjection({
+        type: 'globe' // Set projection to globe
+      });
+      await new Promise(resolve => map.on('idle', resolve));
+      onLoad?.(map);
 
-    <GeoJsonHandler config={options.geoJson} />
-    <ImageSourcesHandler config={options.imageSources} geoJsonConfig={options.geoJson} />
-    {@render children?.()}
-  {/if}
-</MapLibreLoader>
+      rootNode.style.opacity = '1';
+      mapInstance = map;
+      return map;
+    }}
+  >
+    {#if mapInstance}
+      <PanZoomHandler coords={options.coords} z={options.z} bounds={options.bounds} />
+      {#if options.base !== 'satellite'}
+        <MapLabelHandler labels={options.mapLabels} isSatellite={options.base === 'satellite'} />
+      {/if}
+      <MapCustomLabelHandler labels={options.labels} />
+
+      {#if options.base === 'countries'}
+        <MapCountriesBaseHandler />
+        <HighlightCountriesHandler highlightCountries={options.highlightCountries} />
+      {:else if options.base === 'satellite'}
+        <MapRasterHandler
+          url={'https://abcnewsdata.sgp1.digitaloceanspaces.com/map-raster-tiles-bluemarge/{z}/{x}/{y}.webp'}
+          maxZoom={7}
+          attribution="NASA Blue Marble"
+        />
+      {:else}
+        <MapVectorHandler />
+      {/if}
+
+      <GeoJsonHandler config={options.geoJson} />
+      <ImageSourcesHandler config={options.imageSources} geoJsonConfig={options.geoJson} />
+      {@render children?.()}
+    {/if}
+  </MapLibreLoader>
+</div>
 
 <style>
+  .custom-globe {
+    transition: background-color 250ms;
+    width: 100%;
+    height: 100%;
+  }
+
+  .custom-globe--satellite {
+    background-color: #000;
+  }
 </style>
